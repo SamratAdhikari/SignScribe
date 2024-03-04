0c26b927cd922813bd73a2bf0f4024e7399400e2
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-03-04 22:16:39 +0545

    Perhaps the final commit

--- Classification/SignScribe.py
+++ Classification/SignScribe.py
@@ -1,29 +1,47 @@
 import os
 import cv2
+import time
 import math
 import pickle
+import keyboard
 import numpy as np
 import streamlit as st
 import tensorflow as tf
 from HandTrackingModule import HandDetector
 from tensorflow.keras.preprocessing import image
 
+
+
+st.set_page_config(layout="wide")
+
+
 # Load the ASL model
+string = ""
 offset = 20
 img_size = 128
-model = tf.keras.models.load_model('assets/vgg16_model.h5')
+prev_char = None
+start_time = None
+model = tf.keras.models.load_model('assets/model.h5')
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-st.subheader('ASL recognition')
+# st.subheader('ASL recognition')
 
 with open('assets/categories.pkl') as file:
     categories = eval(file.read())
 
+
 gotOutput = False
 string = ''
-string_placeholder = st.empty()
-frame_placeholder = st.empty()
+
+
+
+frame_placeholder, _, image_placeholder = st.columns(3)
+with frame_placeholder:
+    frame_placeholder = st.empty()
+    string_placeholder = st.empty()
+with image_placeholder:
+    image_placeholder = st.image('assets/Figure.png', width=370)
 
 
 
@@ -53,8 +71,6 @@ def backgroundSubtraction(img):
 
 
 while cap.isOpened():
-    # if cv2.waitKey(10) & 0xFF == 27:
-    #     break
 
     success, img = cap.read()
     hands, img = detector.findHands(img)
@@ -93,17 +109,9 @@ while cap.isOpened():
                 hGap = math.ceil((img_size - hCal) / 2)
                 imgWhite[hGap : hCal+hGap, :] = imgResize
 
-
             except Exception as e:
                 pass
 
-
-        try:
-            pass
-            # cv2.imshow('', imgWhite)
-
-        except Exception as e:
-            pass
 
         imgWhite = cv2.flip(imgWhite, 1)
         my_image_arr = image.img_to_array(imgWhite)
@@ -118,17 +126,32 @@ while cap.isOpened():
         cv2.putText(img, char, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (92, 13, 195), 3, cv2.LINE_AA)
         cv2.putText(img, prob, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (92, 13, 195), 3, cv2.LINE_AA)
 
-        gotOutput = True
+        if char == prev_char:
+            if start_time is None:
+                start_time = time.time()
+            elif time.time() - start_time >= 2:  # Check if 2 seconds have passed
+                if char == 'SPACE':
+                    char = "  "
+                string += char
+                start_time = None
+        else:
+            start_time = None
+
+        prev_char = char
+
+
+        if keyboard.is_pressed('s'):
+            if char == 'SPACE':
+                char = "  "
+            string += char
+
+
+
+    # string_placeholder.write(f'Text: {string}')
+    string_placeholder.subheader(f'Text: {string}')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    frame_placeholder.image(img, channels='RGB')
+    frame_placeholder.image(img, channels='RGB', width=700)
 
-# Print the last predicted label before the hand is removed
-print("Last predicted label before hand removal:", char)
-if gotOutput:
-    string += char
-    gotOutput = False
-
-string_placeholder.write(f'Text: {string}')
 
 cap.release()
 cv2.destroyAllWindows()
--- Classification/frontend/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Hello Bitch</div>;
-}
-
-export default App;
